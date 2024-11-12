597d095894c75f63164df1d3954710b00c9b2b39
Nanahide17 <102522276+Nanahide17@users.noreply.github.com>
2024-11-12 17:53:41 +0100

    Reconfiguration BLE

--- app/src/main/java/fr/isen/pretesacque/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/pretesacque/androidsmartdevice/ScanActivity.kt
@@ -4,135 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign.Companion.Center
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import fr.isen.pretesacque.androidsmartdevice.ui.theme.AndroidSmartDeviceTheme
-import fr.isen.pretesacque.androidsmartdevice.ble.BLEManager
+import fr.isen.pretesacque.androidsmartdevice.composable.ScanScreen
 
 class ScanActivity : ComponentActivity() {
-    private lateinit var bleManager: BLEManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        bleManager = BLEManager(this)
+
         setContent {
             AndroidSmartDeviceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ScanScreen(
                         innerPadding = innerPadding,
                         BLE_List = listOf("Bonjour", "Hello", "Guten Tag"),
-                        onStartScan = { bleManager.startScan() },
-                        onStopScan = { bleManager.stopScan() }
+                        onStartScan = TODO(),
+                        onStopScan = TODO()
                     );
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun ScanScreen(
-    innerPadding: PaddingValues,
-    BLE_List: List<String>,
-    onStartScan: () -> Unit,
-    onStopScan: () -> Unit
-) {
-    var scanning by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "SCAN",
-                fontSize = 22.sp,
-                textAlign = Center,
-                modifier = Modifier.padding(innerPadding)
-            )
-        }
-
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(if (scanning) "Scan en cours" else "Lancer la recherche")
-                Image(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .padding(20.dp, 0.dp)
-                        .clickable {
-                            if (scanning) {
-                                onStopScan()
-                            } else {
-                                onStartScan()
-                            }
-                            scanning = !scanning
-                        },
-                    painter = painterResource(if (scanning) R.drawable.pause_blue_button_1 else R.drawable.play_blue_button),
-                    contentDescription = "pause",
-                )
-            }
-
-
-        }
-        if (scanning) {
-            LinearProgressIndicator(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                color = MaterialTheme.colorScheme.secondary,
-                trackColor = MaterialTheme.colorScheme.surfaceVariant
-            )
-        }
-
-        Column {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp) // Espace entre les éléments
-            ) {
-                items(BLE_List) { name ->
-                    Text(text = name)
-                }
-
-            }
-
         }
     }
 }
--- app/src/main/java/fr/isen/pretesacque/androidsmartdevice/composable/ScanComposable.kt
+++ app/src/main/java/fr/isen/pretesacque/androidsmartdevice/composable/ScanComposable.kt
@@ -1,4 +1,4 @@
-package fr.isen.pretesacque.androidsmartdevice.ble
+package fr.isen.pretesacque.androidsmartdevice.composable
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
@@ -6,57 +6,116 @@ import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign.Companion.Center
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import fr.isen.pretesacque.androidsmartdevice.R
 
-class BLEManager(context: Context) {
-    private val bluetoothManager: BluetoothManager =
-        context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-    private val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-    private var bluetoothLeScanner: BluetoothLeScanner? = bluetoothAdapter?.bluetoothLeScanner
+@Composable
+fun ScanScreen(
+    innerPadding: PaddingValues,
+    BLE_List: List<String>,
+    onStartScan: () -> Unit,
+    onStopScan: () -> Unit
+) {
+    var scanning by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "SCAN",
+                fontSize = 22.sp,
+                textAlign = Center,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
 
-    private var isScanning = false
-
-    // Callback pour les résultats de scan BLE
-    private val leScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            result?.let {
-                Log.d("BLEManager", "Device found: ${it.device.name} - ${it.device.address}")
-                // Ici, vous pouvez ajouter des actions pour traiter les résultats (mise à jour de la liste, etc.)
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(if (scanning) "Scan en cours" else "Lancer la recherche")
+                Image(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(20.dp, 0.dp)
+                        .clickable {
+                            if (scanning) {
+                                onStopScan()
+                            } else {
+                                onStartScan()
+                            }
+                            scanning = !scanning
+                        },
+                    painter = painterResource(if (scanning) R.drawable.pause_blue_button_1 else R.drawable.play_blue_button),
+                    contentDescription = "pause",
+                )
             }
+
+
+        }
+        if (scanning) {
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant
+            )
         }
 
-        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
-            results?.forEach { result ->
-                Log.d("BLEManager", "Device found: ${result.device.name} - ${result.device.address}")
+        Column {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp) // Espace entre les éléments
+            ) {
+                items(BLE_List) { name ->
+                    Text(text = name)
+                }
+
             }
-        }
 
-        override fun onScanFailed(errorCode: Int) {
-            Log.e("BLEManager", "Scan failed with error: $errorCode")
         }
-    }
-
-    // Fonction pour démarrer le scan BLE
-    fun startScan() {
-        if (!isScanning) {
-            bluetoothLeScanner?.startScan(leScanCallback)
-            isScanning = true
-            Log.d("BLEManager", "Scan started")
-        }
-    }
-
-    // Fonction pour arrêter le scan BLE
-    fun stopScan() {
-        if (isScanning) {
-            bluetoothLeScanner?.stopScan(leScanCallback)
-            isScanning = false
-            Log.d("BLEManager", "Scan stopped")
-        }
-    }
-
-    // Vérifie si le Bluetooth est activé
-    fun isBluetoothEnabled(): Boolean {
-        return bluetoothAdapter?.isEnabled == true
     }
 }
 
